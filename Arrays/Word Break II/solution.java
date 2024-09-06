aafc8fbac5e2dc3143c9222f5d828588197d6d32
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-09-06 20:10:56 +0530

    Add files via upload

---
+++ Arrays/Word Break II/solution.java
@@ -0,0 +1,28 @@
+class Trie {
+    Trie[] children = new Trie[26];
+    boolean isEnd;
+
+    void insert(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) {
+                node.children[c] = new Trie();
+            }
+            node = node.children[c];
+        }
+        node.isEnd = true;
+    }
+
+    boolean search(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) {
+                return false;
+            }
+            node = node.children[c];
+        }
+        return node.isEnd;
+    }
+}
