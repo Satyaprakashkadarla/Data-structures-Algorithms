59cabcbd2073927286d0c39edb40904c37b4ea8e
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-02-08 16:41:07 +0530

    Add files via upload

--- Arrays/Maximum Product Subarray/solution.java
+++ Arrays/Maximum Product Subarray/solution.java
@@ -1,12 +1,18 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int f = nums[0], g = nums[0], ans = nums[0];
-        for (int i = 1; i < nums.length; ++i) {
-            int ff = f, gg = g;
-            f = Math.max(nums[i], Math.max(ff * nums[i], gg * nums[i]));
-            g = Math.min(nums[i], Math.min(ff * nums[i], gg * nums[i]));
-            ans = Math.max(ans, f);
+    public int maxProduct(int[] arr) {
+        int maxEnd = arr[0], minEnd = arr[0], maxProd = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                int temp = maxEnd;
+                maxEnd = minEnd;
+                minEnd = temp;
+            }
+            maxEnd = Math.max(arr[i], maxEnd * arr[i]);
+            minEnd = Math.min(arr[i], minEnd * arr[i]);
+            maxProd = Math.max(maxProd, maxEnd);
         }
-        return ans;
+        
+        return maxProd;
     }
 }
