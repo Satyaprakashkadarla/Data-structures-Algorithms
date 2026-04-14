4b50309e99401ae486bed39001fe5bb41c2eb1e3
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-04-14 07:01:01 +0530

    Add files via upload

--- Arrays/Minimum Total Distance Traveled/solution.java
+++ Arrays/Minimum Total Distance Traveled/solution.java
@@ -1,25 +1,37 @@
 class Solution {
-  public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-    Collections.sort(robot);
-    Arrays.sort(factory, (a, b) -> Integer.compare(a[0], b[0]));
-    long[][][] mem = new long[robot.size()][factory.length][robot.size()];
-    return minimumTotalDistance(robot, factory, 0, 0, 0, mem);
-  }
+    private Long[][] f;
+    private List<Integer> robot;
+    private int[][] factory;
 
-  private long minimumTotalDistance(List<Integer> robot, int[][] factory, int i, int j, int k,
-                                    long[][][] mem) {
-    if (i == robot.size())
-      return 0;
-    if (j == factory.length)
-      return Long.MAX_VALUE;
-    if (mem[i][j][k] > 0)
-      return mem[i][j][k];
-    final long skipFactory = minimumTotalDistance(robot, factory, i, j + 1, 0, mem);
-    final int position = factory[j][0];
-    final int limit = factory[j][1];
-    final long useFactory = limit > k ? minimumTotalDistance(robot, factory, i + 1, j, k + 1, mem) +
-                                            Math.abs(robot.get(i) - position)
-                                      : Long.MAX_VALUE / 2;
-    return mem[i][j][k] = Math.min(skipFactory, useFactory);
-  }
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        Collections.sort(robot);
+        Arrays.sort(factory, (a, b) -> a[0] - b[0]);
+        this.robot = robot;
+        this.factory = factory;
+        f = new Long[robot.size()][factory.length];
+        return dfs(0, 0);
+    }
+
+    private long dfs(int i, int j) {
+        if (i == robot.size()) {
+            return 0;
+        }
+        if (j == factory.length) {
+            return Long.MAX_VALUE / 1000;
+        }
+        if (f[i][j] != null) {
+            return f[i][j];
+        }
+        long ans = dfs(i, j + 1);
+        long t = 0;
+        for (int k = 0; k < factory[j][1]; ++k) {
+            if (i + k == robot.size()) {
+                break;
+            }
+            t += Math.abs(robot.get(i + k) - factory[j][0]);
+            ans = Math.min(ans, t + dfs(i + k + 1, j + 1));
+        }
+        f[i][j] = ans;
+        return ans;
+    }
 }
