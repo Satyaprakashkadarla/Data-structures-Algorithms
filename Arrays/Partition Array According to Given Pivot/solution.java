1cca7728b888f1834b558da8508d37bde6d3c419
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-06-08 07:05:59 +0530

    Add files via upload

--- Arrays/Partition Array According to Given Pivot/solution.java
+++ Arrays/Partition Array According to Given Pivot/solution.java
@@ -1,20 +1,30 @@
 class Solution {
-  public int[] pivotArray(int[] nums, int pivot) {
-    int[] ans = new int[nums.length];
-    int i = 0; // ans' index
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int idx = 0;
 
-    for (final int num : nums)
-      if (num < pivot)
-        ans[i++] = num;
+        // Elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                res[idx++] = num;
+            }
+        }
 
-    for (final int num : nums)
-      if (num == pivot)
-        ans[i++] = num;
+        // Elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                res[idx++] = num;
+            }
+        }
 
-    for (final int num : nums)
-      if (num > pivot)
-        ans[i++] = num;
+        // Elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                res[idx++] = num;
+            }
+        }
 
-    return ans;
-  }
+        return res;
+    }
 }
