33e853ffca7e17c598164aef134db6bc2f74eae1
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-09-08 05:43:00 +0530

    Add files via upload

---
+++ Mathematics/Count Commas in Range/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCommas(int n) {
+        int total = 0;
+        int power = 1000;
+
+        while (power <= n) {
+            total += n - power + 1;
+            power *= 1000;
+        }
+
+        return total;
+    }
+}
